a516a0f83bc020071491abe5dd5bf246d55d01c0
Isaac Jiménez Merino <113287554+Isaac-JM@users.noreply.github.com>
2024-02-28 10:32:48 +0100

    Redirigir al juego pulsando Enter

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -18,6 +18,13 @@ export class HomeView extends LitElement {
   constructor() {
     super()
     this.messageLogin = {}
+    this.addEventListener('keyup', this.handleInputKeyUp.bind(this));
+  }
+
+  handleInputKeyUp(event) {
+    if (event.key === 'Enter') {
+      this.newPlayer()
+    }
   }
 
   shouldUpdate(changedProperties) {
@@ -58,19 +65,19 @@ export class HomeView extends LitElement {
               </div>
 
                   <div>
-                  <form class="space-y-6" action="#">
+                  <div class="space-y-6" action="#">
                   <div class="flex justify-center">
                     <img src="./logo_game.png" class="h-[200px] w-[200px]">
                   </div>
                       <div>
                           <label for="player" class="block mb-2 text-sm font-medium text-gray-900 ">Name</label>
-                          <input @click="${(e) => this.messageLogin = {}}" id="new-player" type="text" name="player" id="player" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Name" required />
+                          <input @click="${(e) => this.messageLogin = {}}"  id="new-player" type="text" name="player" id="player" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Name" required />
                       </div>
                       <div class="text-sm font-medium ${this.messageLogin.color ? this.messageLogin.color : ''}">
                       ${this.messageLogin.message ? this.messageLogin.message : ''}
                       </div>
                       <button @click="${this.newPlayer}" type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Start</button>
-                  </form>
+                  </div>
               </div>
           </div>
     </div>  
